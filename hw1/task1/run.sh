211a775cef56eb3a4b7465d2fa4c524e4f46662f
ferrolho <henriqueferrolho@gmail.com>
2016-10-18 11:49:40 +0100

    working on task1

---
+++ hw1/task1/run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+hdfs dfs -rm -r /user/$USER/assignment1/task1
+hadoop jar /opt/hadoop/hadoop-2.7.3/share/hadoop/tools/lib/hadoop-streaming-2.7.3.jar \
+ -D mapred.job.name="Task 1 - s1683857" \
+ -input /data/assignments/ex1/webSmall.txt \
+ -output /user/$USER/assignment1/task1 \
+ -mapper mapper.py \
+ -file mapper.py
