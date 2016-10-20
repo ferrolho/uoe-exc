b8cfd1e7593073ba876016af717b2cd49f53ae73
ferrolho <henriqueferrolho@gmail.com>
2016-10-21 00:47:14 +0100

    Fixes script

--- hw1/task1/run.sh
+++ hw1/task1/run.sh
@@ -5,5 +5,5 @@ hadoop jar /opt/hadoop/hadoop-2.7.3/share/hadoop/tools/lib/hadoop-streaming-2.7.
  -input /data/assignments/ex1/webLarge.txt \
  -output /user/$USER/assignment1/task1 \
  -mapper mapper.py \
- -file mapper.py
+ -file mapper.py \
  -reducer NONE
