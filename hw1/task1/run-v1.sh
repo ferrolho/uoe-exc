4f35d41f354b3e99865ff0cf3704dc8c2b122005
ferrolho <henriqueferrolho@gmail.com>
2016-10-21 00:23:20 +0100

    Wokring with large dataset for task1 now

--- hw1/task1/run-v1.sh
+++ hw1/task1/run-v1.sh
@@ -2,7 +2,7 @@
 hdfs dfs -rm -r /user/$USER/assignment1/task1
 hadoop jar /opt/hadoop/hadoop-2.7.3/share/hadoop/tools/lib/hadoop-streaming-2.7.3.jar \
  -D mapred.job.name="Task 1 - s1683857" \
- -input /data/assignments/ex1/webSmall.txt \
+ -input /data/assignments/ex1/webLarge.txt \
  -output /user/$USER/assignment1/task1 \
  -mapper mapper.py \
  -file mapper.py
