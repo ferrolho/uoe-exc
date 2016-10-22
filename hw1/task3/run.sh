10b1c14ec9de5e5d1ffc8df3f50ec7728b6398e8
ferrolho <henriqueferrolho@gmail.com>
2016-10-22 17:26:14 +0100

    Fixes task3 run.sh

--- hw1/task3/run.sh
+++ hw1/task3/run.sh
@@ -2,9 +2,9 @@
 hdfs dfs -rm -r /user/$USER/assignment1/task3
 hadoop jar /opt/hadoop/hadoop-2.7.3/share/hadoop/tools/lib/hadoop-streaming-2.7.3.jar \
  -D mapreduce.job.name="Task 3 - s1683857" \
- -D mapreduce.reduce.tasks=1 \
+ -D mapreduce.job.reduces=1 \
  -files mapper.py,reducer.py \
- -input /user/$USER/assignment1/task2/part-* \
+ -input  /user/$USER/assignment1/task2/part-* \
  -output /user/$USER/assignment1/task3 \
  -mapper mapper.py \
  -combiner reducer.py \
