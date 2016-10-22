bc06f937dcc95f334082db2b893e9ea0810e01c3
ferrolho <henriqueferrolho@gmail.com>
2016-10-22 13:41:47 +0100

    Improves run scripts

--- hw1/task1/run.sh
+++ hw1/task1/run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 hdfs dfs -rm -r /user/$USER/assignment1/task1
 hadoop jar /opt/hadoop/hadoop-2.7.3/share/hadoop/tools/lib/hadoop-streaming-2.7.3.jar \
- -D mapred.job.name="Task 1 - s1683857" \
+ -D mapreduce.job.name="Task 1 - s1683857" \
+ -files mapper.py \
  -input /data/assignments/ex1/webLarge.txt \
  -output /user/$USER/assignment1/task1 \
  -mapper mapper.py \
- -file mapper.py \
  -reducer NONE
--- hw1/task2/run.sh
+++ hw1/task2/run.sh
@@ -3,7 +3,7 @@ hdfs dfs -rm -r /user/$USER/assignment1/task2
 hadoop jar /opt/hadoop/hadoop-2.7.3/share/hadoop/tools/lib/hadoop-streaming-2.7.3.jar \
  -D mapreduce.job.name="Task 2 - s1683857" \
  -files mapper.py,reducer.py \
- -input /user/$USER/assignment1/task1 \
+ -input /user/$USER/assignment1/task1/part-* \
  -output /user/$USER/assignment1/task2 \
  -mapper mapper.py \
  -reducer reducer.py
