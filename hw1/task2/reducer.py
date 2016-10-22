978bb7dd82a86516ca2155b21016526f52b7c0da
ferrolho <henriqueferrolho@gmail.com>
2016-10-22 01:17:34 +0100

    Task 2 done

---
+++ hw1/task2/reducer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import sys
+
+prev_line = ""
+
+for line in sys.stdin:
+	line = line.strip()
+
+	if prev_line == line:
+		line_occurrences += 1
+	else:
+		if prev_line and line_occurrences == 1:
+			print(prev_line)
+
+		line_occurrences = 1
+		prev_line = line
+
+# Don't forget the last line
+if prev_line == line and line_occurrences == 1:
+	print(prev_line)
