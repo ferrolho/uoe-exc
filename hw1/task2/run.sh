975de6f5ab9fe085aa7292ab62af6ff5527fdc1a
ferrolho <henriqueferrolho@gmail.com>
2016-10-22 01:30:29 +0100

    Edits task 2 run.sh

--- hw1/task2/run.sh
+++ hw1/task2/run.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 hdfs dfs -rm -r /user/$USER/assignment1/task2
 hadoop jar /opt/hadoop/hadoop-2.7.3/share/hadoop/tools/lib/hadoop-streaming-2.7.3.jar \
- -D mapred.job.name="Task 2 - s1683857" \
+ -D mapreduce.job.name = "Task 2 - s1683857" \
+ -D files = mapper.py, reducer.py
  -input /user/$USER/assignment1/task1 \
  -output /user/$USER/assignment1/task2 \
  -mapper mapper.py \
- -file mapper.py \
- -reducer reducer.py \
- -file reducer.py
+ -reducer reducer.py
