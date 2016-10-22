5b087807658b16a8cce737fd76af185657daee0f
ferrolho <henriqueferrolho@gmail.com>
2016-10-22 18:35:36 +0100

    Nothing to see here

--- hw1/task5/run.sh
+++ hw1/task5/run.sh
@@ -3,7 +3,7 @@ hdfs dfs -rm -r /user/$USER/assignment1/task5
 hadoop jar /opt/hadoop/hadoop-2.7.3/share/hadoop/tools/lib/hadoop-streaming-2.7.3.jar \
  -D mapreduce.job.name="Task 5 - s1683857" \
  -files mapper.py,reducer.py \
- -input /user/$USER/assignment1/task4/part-* \
+ -input  /user/$USER/assignment1/task4/part-* \
  -output /user/$USER/assignment1/task5 \
  -mapper mapper.py \
  -reducer reducer.py
--- hw1/task7/run.sh
+++ hw1/task7/run.sh
@@ -3,7 +3,7 @@ hdfs dfs -rm -r /user/$USER/assignment1/task7
 hadoop jar /opt/hadoop/hadoop-2.7.3/share/hadoop/tools/lib/hadoop-streaming-2.7.3.jar \
  -D mapreduce.job.name="Task 7 - s1683857" \
  -files mapper.py,reducer.py \
- -input /user/$USER/assignment1/task6/part-* \
+ -input  /user/$USER/assignment1/task6/part-* \
  -output /user/$USER/assignment1/task7 \
  -mapper mapper.py \
  -reducer reducer.py
