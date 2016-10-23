1192fdc96744cdefeb805e94a3b909738c6bea97
ferrolho <henriqueferrolho@gmail.com>
2016-10-23 04:28:39 +0100

    Fixes sort in task 5

--- hw1/task5/run.sh
+++ hw1/task5/run.sh
@@ -4,6 +4,9 @@ hdfs dfs -rm -r /user/$USER/assignment1/task5
 
 hadoop jar /opt/hadoop/hadoop-2.7.3/share/hadoop/tools/lib/hadoop-streaming-2.7.3.jar \
  -D mapreduce.job.name="Task 5 - s1683857" \
+ -D mapreduce.job.output.key.comparator.class=org.apache.hadoop.mapreduce.lib.partition.KeyFieldBasedComparator \
+ -D mapreduce.partition.keycomparator.options="-k1,1nr" \
+ -D mapreduce.job.reduces=1 \
  -files mapper.py,reducer.py \
  -input  /user/$USER/temp/input.in \
  -output /user/$USER/assignment1/task5 \
