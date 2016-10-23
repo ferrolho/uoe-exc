fe2121a5d0ea8eddd3b9c024707fab6c0fe5273d
ferrolho <henriqueferrolho@gmail.com>
2016-10-23 19:17:12 +0100

    Adds task 6

--- hw1/task6/reducer.py
+++ hw1/task6/reducer.py
@@ -1,1 +1,41 @@
 #!/usr/bin/python3
+
+from math import log2
+import sys
+
+def parseSequence(seq):
+	delimiter = ' '
+	groups = seq.split(delimiter)
+	return delimiter.join(groups[:2]), delimiter.join(groups[2:])
+
+def printResults():
+	for i in range(len(pwcList)):
+		pwc = pwcList[i] / count_total
+		pwcList[i] = - pwc * log2(pwc)
+
+	entropy = sum(pwcList)
+	print(prev_context, entropy)
+
+context = ""
+prev_context = ""
+count_total = 0
+pwcList = []
+
+for line in sys.stdin:
+	seq, count = line.strip().split('\t')
+	context, follower = parseSequence(seq)
+	count = int(count)
+
+	if prev_context == context:
+		count_total += count
+		pwcList.append(count)
+	else:
+		if prev_context:
+			printResults()
+
+		prev_context = context
+		count_total = count
+		pwcList = [count]
+
+if prev_context == context:
+	printResults()
