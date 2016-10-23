aa2268debe89341c484e110d68aeb949ce74a445
ferrolho <henriqueferrolho@gmail.com>
2016-10-23 01:45:28 +0100

    Improves task 3 script

--- hw1/task3/run.sh
+++ hw1/task3/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# 1st job
+## 1st job ##
 
 hdfs dfs -rm -r /user/$USER/assignment1/task3-temp
 
@@ -8,18 +8,21 @@ hadoop jar /opt/hadoop/hadoop-2.7.3/share/hadoop/tools/lib/hadoop-streaming-2.7.
  -D mapreduce.job.name="Task 3 (Pt. 1) - s1683857" \
  -files mapper.py,reducer.py \
  -input  /user/$USER/assignment1/task2/part-* \
- -output /user/$USER/assignment1/task3-temp \ # store 1st job output on temp folder
+ # store 1st job output on temp folder
+ -output /user/$USER/assignment1/task3-temp \
  -mapper mapper.py \
- -combiner reducer.py \ # reducer code can be used as combiner
+ # reducer code can be used as combiner
+ -combiner reducer.py \
  -reducer reducer.py
 
-# 2nd job
+## 2nd job ##
 
 hdfs dfs -rm -r /user/$USER/assignment1/task3
 
 hadoop jar /opt/hadoop/hadoop-2.7.3/share/hadoop/tools/lib/hadoop-streaming-2.7.3.jar \
  -D mapreduce.job.name="Task 3 (Pt. 2) - s1683857" \
- -D mapreduce.job.reduces=1 \ # run 1 reducer only
+ # run 1 reducer only
+ -D mapreduce.job.reduces=1 \
  -files reducer.py \
  -input  /user/$USER/assignment1/task3-temp/part-* \
  -output /user/$USER/assignment1/task3 \
--- hw1/task4/run.sh
+++ hw1/task4/run.sh
@@ -2,7 +2,6 @@
 hdfs dfs -rm -r /user/$USER/assignment1/task4
 hadoop jar /opt/hadoop/hadoop-2.7.3/share/hadoop/tools/lib/hadoop-streaming-2.7.3.jar \
  -D mapreduce.job.name="Task 4 - s1683857" \
- -D mapreduce.job.reduces=10 \
  -files mapper.py,reducer.py \
  -input  /user/$USER/assignment1/task2/part-* \
  -output /user/$USER/assignment1/task4 \
