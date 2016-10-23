20f982173e41af8c1296951a110f477aa2c2b6bc
ferrolho <henriqueferrolho@gmail.com>
2016-10-23 02:03:17 +0100

    Ok. Going to task 5

--- hw1/task4/run.sh
+++ hw1/task4/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+
 hdfs dfs -rm -r /user/$USER/assignment1/task4
+
 hadoop jar /opt/hadoop/hadoop-2.7.3/share/hadoop/tools/lib/hadoop-streaming-2.7.3.jar \
  -D mapreduce.job.name="Task 4 - s1683857" \
  -files mapper.py,reducer.py \
@@ -8,3 +10,9 @@ hadoop jar /opt/hadoop/hadoop-2.7.3/share/hadoop/tools/lib/hadoop-streaming-2.7.
  -mapper mapper.py \
  -combiner reducer.py \
  -reducer reducer.py
+
+#
+# Note:
+#   Running a combiner might make things slower for small inputs.
+#   But it is better for much larger inputs.
+#
