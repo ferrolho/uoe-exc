75ee85e55e20e1a705107bb3d78cf575f5fbd4ce
ferrolho <henriqueferrolho@gmail.com>
2016-10-23 01:19:28 +0100

    Testing something on task 3

--- hw1/task3/run.sh
+++ hw1/task3/run.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
+hdfs dfs -rm -r /user/$USER/assignment1/task3-temp
+hadoop jar /opt/hadoop/hadoop-2.7.3/share/hadoop/tools/lib/hadoop-streaming-2.7.3.jar \
+ -D mapreduce.job.name="Task 3 - s1683857" \
+ -files mapper.py,reducer.py \
+ -input  /user/$USER/assignment1/task2/part-* \
+ -output /user/$USER/assignment1/task3-temp \
+ -mapper mapper.py \
+ -combiner reducer.py \
+ -reducer reducer.py
 hdfs dfs -rm -r /user/$USER/assignment1/task3
 hadoop jar /opt/hadoop/hadoop-2.7.3/share/hadoop/tools/lib/hadoop-streaming-2.7.3.jar \
  -D mapreduce.job.name="Task 3 - s1683857" \
  -D mapreduce.job.reduces=1 \
- -files mapper.py,reducer.py \
- -input  /user/$USER/assignment1/task2/part-* \
+ -files reducer.py \
+ -input  /user/$USER/assignment1/task3-temp/part-* \
  -output /user/$USER/assignment1/task3 \
- -mapper mapper.py \
- -combiner reducer.py \
+ -mapper cat \
  -reducer reducer.py
