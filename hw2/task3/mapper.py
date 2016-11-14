db6b173e62e7910583167106988a1d2bba0a3192
ferrolho <henriqueferrolho@gmail.com>
2016-11-14 15:40:04 +0000

    Starts task 3

---
+++ hw2/task3/mapper.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+from heapq import heappush, heappushpop
+import sys
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+def parsePost(xml):
+	tokens = xml.strip().split()
+	result = dict()
+
+	for token in tokens:
+		key_value = token.split('=')
+		if len(key_value) == 2:
+			key, value = key_value
+			if key and value:
+				result[key] = value.strip('"')
+
+	return result
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+def postIsQuestion(post):
+	return post['PostTypeId'] == '1'
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+top10 = []
+
+for line in sys.stdin:
+	post = parsePost(line)
+
+	if postIsQuestion(post):
+		count = int(post['ViewCount'])
+
+		if len(top10) < 10:
+			heappush(top10, (count, post['Id']))
+		else:
+			heappushpop(top10, (count, post['Id']))
+
+for count, id in top10:
+	print(count, id, sep='\t')
---
+++ hw2/task3/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+hdfs dfs -rm -r /user/$USER/assignment2/task3
+
+hadoop jar /opt/hadoop/hadoop-2.7.3/share/hadoop/tools/lib/hadoop-streaming-2.7.3.jar \
+  -D mapreduce.job.name="Task 3 - s1683857" \
+  -D mapreduce.job.output.key.comparator.class=org.apache.hadoop.mapreduce.lib.partition.KeyFieldBasedComparator \
+  -D mapreduce.partition.keycomparator.options="-nr" \
+  -D mapreduce.job.reduces=1 \
+  -files mapper.py,reducer.py \
+ -input  /data/assignments/ex2/part2/stackLarge.txt \
+ -output /user/$USER/assignment2/task3 \
+ -mapper  mapper.py \
+ -reducer reducer.py
+
+hdfs dfs -cat /user/$USER/assignment2/task3/part-* | head -20 > output.out
