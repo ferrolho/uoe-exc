5480418da64115b36bfc605b73398ccf1d9d9777
ferrolho <henriqueferrolho@gmail.com>
2016-11-14 20:49:57 +0000

    Fixing task 3

--- hw2/task3/reducer2.py
+++ hw2/task3/reducer2.py
@@ -6,15 +6,33 @@ best_userId = None
 best_userQuestionIds = None
 best_userQuestionIdsLength = 0
 
+prev_userId = None
+userQuestionIds = set()
+
 for line in sys.stdin:
 	userId_userQuestionIds = line.strip().split('\t')
 
 	if len(userId_userQuestionIds) == 2:
-		userId, userQuestionIds = userId_userQuestionIds
+		userId, userQuestionIdsStr = userId_userQuestionIds
 
-		if len(userQuestionIds.split(', ')) > best_userQuestionIdsLength:
-			best_userId = userId
-			best_userQuestionIds = userQuestionIds
-			best_userQuestionIdsLength = len(userQuestionIds.split(', '))
+		if userId == prev_userId:
+			userQuestionIds.update(list(map(int, userQuestionIdsStr.split(', '))))
+		else:
+			if prev_userId:
+				if len(userQuestionIds) > best_userQuestionIdsLength:
+					best_userId = prev_userId
+					best_userQuestionIds = set(userQuestionIds)
+					best_userQuestionIdsLength = len(userQuestionIds)
 
-print(best_userId, best_userQuestionIds, sep=' -> ')
+			userQuestionIds.clear()
+			userQuestionIds.update(list(map(int, userQuestionIdsStr.split(', '))))
+			prev_userId = userId
+
+# Don't forget the last line
+if userId == prev_userId:
+	if len(userQuestionIds) > best_userQuestionIdsLength:
+		best_userId = prev_userId
+		best_userQuestionIds = set(userQuestionIds)
+		best_userQuestionIdsLength = len(userQuestionIds)
+
+print(best_userId, str(sorted(best_userQuestionIds)).strip('[]'), sep='\t')
