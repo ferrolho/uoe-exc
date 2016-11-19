ebed1e78a3e955e63d90088cdf253875ddc66152
ferrolho <henriqueferrolho@gmail.com>
2016-11-19 19:13:43 +0000

    Edits Task 6 run.sh

--- hw2/task6/run.sh
+++ hw2/task6/run.sh
@@ -1,1 +1,9 @@
-./task6.py < /afs/inf.ed.ac.uk/group/teaching/exc/ex2/part3/webLarge.txt | head -20 > output.out
+#!/bin/bash
+
+./task6.py < /afs/inf.ed.ac.uk/group/teaching/exc/ex2/part3/webLarge.txt > output-temp.out
+
+hdfs dfs -copyFromLocal output-temp.out /user/$USER/assignment2/task6/output.out
+
+cat output-temp.out | head -20 > output.out
+
+rm output-temp.out
