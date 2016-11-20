83d4a08858e130301dfd63f157572bd258e70e8f
ferrolho <henriqueferrolho@gmail.com>
2016-11-20 01:53:00 +0000

    Adds Task 7 output.out

--- hw2/task7/run.sh
+++ hw2/task7/run.sh
@@ -4,6 +4,8 @@ INPUT=/afs/inf.ed.ac.uk/group/teaching/exc/ex2/part3/webLarge.txt
 
 ./task7.py $(cat $INPUT | wc -l) < $INPUT > output-temp.out
 
+hdfs dfs -rm /user/$USER/assignment2/task7/output.out
+
 hdfs dfs -copyFromLocal output-temp.out /user/$USER/assignment2/task7/output.out
 
 cat output-temp.out | head -20 > output.out
