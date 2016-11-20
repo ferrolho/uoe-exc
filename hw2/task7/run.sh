453e9645b1a85e1b8ae171b9b6afc0c263b49386
ferrolho <henriqueferrolho@gmail.com>
2016-11-20 01:26:13 +0000

    Adds task 7

---
+++ hw2/task7/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+INPUT=/afs/inf.ed.ac.uk/group/teaching/exc/ex2/part3/webLarge.txt
+
+./task7.py $(cat $INPUT | wc -l) < $INPUT > output-temp.out
+
+hdfs dfs -copyFromLocal output-temp.out /user/$USER/assignment2/task7/output.out
+
+cat output-temp.out | head -20 > output.out
+
+rm output-temp.out
---
+++ hw2/task7/task7.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+
+from bitarray import bitarray
+import math
+import random
+import sys
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# False positive rate
+FPR = 0.01
+
+# Globals
+filter = None
+k = None
+m = None
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+def eprint(*args, **kwargs):
+	print(*args, file=sys.stderr, **kwargs)
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# Adds key to the set
+def insert(key):
+	for i in range(k):
+		hash_key = '{}_{}'.format(i, key)
+		filter_pos = hash(hash_key) % m
+		filter[filter_pos] = 1
+
+# If key is *definitely not* in the set, return 'False'.
+# Returning 'True' means it *may be* or not in the set - do costly check.
+def query(key):
+	for i in range(k):
+		hash_key = '{}_{}'.format(i, key)
+		filter_pos = hash(hash_key) % m
+		if not filter[filter_pos]:
+			return False
+	return True
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+if len(sys.argv) < 2:
+
+	eprint('Error: not enough arguments')
+	eprint('Usage:', sys.argv[0], 'N')
+	eprint('  N - Total number of lines in the input file')
+
+else:
+
+	# Elements to be inserted in the filter
+	n = int(sys.argv[1])
+
+	# Bits per key
+	bitsPerKey = math.ceil(- math.log2(FPR) / math.log(2))
+
+	# Bits in filter
+	m = n * bitsPerKey
+
+	# Create the bitarray
+	filter = bitarray(m)
+	filter.setall(0)
+
+	# Optimal no. of hashing functions
+	k = math.ceil((m / n) * math.log(2))
+
+	# Approximately de-duplicate lines in input file
+	for line in sys.stdin:
+		line = line.strip()
+
+		if not query(line):
+			insert(line)
+			print(line)
